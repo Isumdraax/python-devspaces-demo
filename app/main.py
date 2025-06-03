fd3b818b947723c92f960c28b4e6cbc58f73d2e8
Isumdraax <59834217+Isumdraax@users.noreply.github.com>
2025-06-03 14:35:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,7 @@
-"""Top-level implementation of the helloworld program."""
+from fastapi import FastAPI
 
-import argparse
-import sys
+app = FastAPI()
 
-import helloworld
-
-
-parser = argparse.ArgumentParser(
-        description='A simple example program to print a friendly greeting.')
-parser.add_argument('--version', action='version',
-        version='helloworld ' + helloworld.__version__)
-
-
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-
-    # The helloworld program doesn't expect any arguments.
-    # This just checks for the special --version and --help arguments and
-    # ensures the user hasn't passed any other unrecognized arguments.
-    parser.parse_args(argv[1:])
-
-    print("Hello, world")
-
-    return 0
+@app.get("/")
+async def root():
+    return {"message": "Welcome to FastAPI"}
